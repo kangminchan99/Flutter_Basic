8cfab84ab182a6fddc6651505ccfc9bf9f2b7ef2
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-08-29 00:02:29 +0900

    gridview.builder

--- lib/screens/grid_view_screen.dart
+++ lib/screens/grid_view_screen.dart
@@ -8,36 +8,43 @@ class GridViewScreen extends StatefulWidget {
 }
 
 class _GridViewScreenState extends State<GridViewScreen> {
+  final gridList = [
+    {
+      'number': 'sample title1',
+      'color': Colors.green,
+    },
+    {
+      'number': 'sample title2',
+      'color': Colors.red,
+    },
+    {
+      'number': 'sample title3',
+      'color': Colors.yellow,
+    },
+    {
+      'number': 'sample title4',
+      'color': Colors.pink,
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GridView'),
-      ),
-      body: GridView(
-        scrollDirection: Axis.horizontal,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          // crossAxisSpacing - 열 사이의 공백을 나타냄.
-          crossAxisSpacing: 15,
-          // mainAxisSpacing - 행 사이의 공백을 나타냄.
-          mainAxisSpacing: 15,
+        appBar: AppBar(
+          title: const Text('GridView'),
         ),
-        children: [
-          girdItem(number: '1', choiceColor: Colors.red),
-          girdItem(number: '2', choiceColor: Colors.pink),
-          girdItem(number: '3', choiceColor: Colors.yellow),
-          girdItem(number: '4', choiceColor: Colors.green),
-          girdItem(number: '5', choiceColor: Colors.green),
-          girdItem(number: '6', choiceColor: Colors.green),
-          girdItem(number: '7', choiceColor: Colors.green),
-          girdItem(number: '8', choiceColor: Colors.green),
-          girdItem(number: '9', choiceColor: Colors.green),
-          girdItem(number: '10', choiceColor: Colors.green),
-          girdItem(number: '11', choiceColor: Colors.green),
-        ],
-      ),
-    );
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+          ),
+          itemCount: gridList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return girdItem(
+                number: gridList[index]['number'] as String,
+                choiceColor: gridList[index]['color'] as Color);
+          },
+        ));
   }
 
   Widget girdItem({String number = '0', Color choiceColor = Colors.blue}) {
